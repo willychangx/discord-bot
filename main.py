28310294f097c616f77ea15e72f0b4f0dcf8b9fc
willychangx <willychang@rocketmail.com>
2021-01-15 16:41:22 -1000

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.author.id == 799433632129089567: # 365372273990959104:
+        await discord.Message.delete(message)
+        await message.channel.send("I'm Frendy's mortal enemy!")
+    # if message.content.startswith('$hello'):
+    #     await message.channel.send(message.author)
+
+client.run(os.getenv('TOKEN'))
